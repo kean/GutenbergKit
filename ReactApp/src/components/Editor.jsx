a85a31b770c2335c4d4838e446a5e052fe6963d3
David Calhoun <github@davidcalhoun.me>
2024-07-18 11:02:41 -0400

    feat: Load Jetpack blocks

--- ReactApp/src/components/Editor.jsx
+++ ReactApp/src/components/Editor.jsx
@@ -12,7 +12,8 @@ import {
 import { Popover } from '@wordpress/components';
 import { getBlockTypes, unregisterBlockType } from '@wordpress/blocks';
 import { registerCoreBlocks } from '@wordpress/block-library';
-import { parse, serialize, registerBlockType } from '@wordpress/blocks';
+import { parse, serialize, store as blocksStore } from '@wordpress/blocks';
+import { dispatch } from '@wordpress/data';
 
 // Default styles that are needed for the editor.
 import '@wordpress/components/build-style/style.css';
@@ -30,10 +31,93 @@ import '@wordpress/format-library/build-style/style.css';
 // Internal imports
 import EditorToolbar from './EditorToolbar';
 import { postMessage } from '../misc/Helpers';
+import { __unstableResolvedAssets } from './BlocksAssets';
 // import CodeEditor from './CodeEditor';
 
 // Current editor (assumes can be only one instance).
 let editor = {};
+
+import * as react from 'react';
+import * as reactDom from 'react-dom';
+import * as wpBlocks from '@wordpress/blocks';
+import * as wpElement from '@wordpress/element';
+import * as wpPrimitives from '@wordpress/primitives';
+import * as wpCompose from '@wordpress/compose';
+import * as wpData from '@wordpress/data';
+import * as wpBlockEditor from '@wordpress/block-editor';
+import * as wpI18n from '@wordpress/i18n';
+import * as wpComponents from '@wordpress/components';
+import * as wpHooks from '@wordpress/hooks';
+import * as wpUrl from '@wordpress/url';
+import * as wpEditSite from '@wordpress/edit-site';
+import apiFetch from '@wordpress/api-fetch';
+import * as wpDate from '@wordpress/date';
+import domReady from '@wordpress/dom-ready';
+import * as wpPlugins from '@wordpress/plugins';
+import * as wpViewport from '@wordpress/viewport';
+import * as wpRichText from '@wordpress/rich-text';
+import * as wpEditor from '@wordpress/editor';
+import * as wpCoreData from '@wordpress/core-data';
+import * as wpBlob from '@wordpress/blob';
+import TokenList from '@wordpress/token-list';
+
+window.wp = window.wp || {};
+window.wp.blocks = wpBlocks;
+window.wp.element = wpElement;
+window.wp.primitives = wpPrimitives;
+window.wp.compose = wpCompose;
+window.wp.data = wpData;
+window.wp.blockEditor = wpBlockEditor;
+window.wp.i18n = wpI18n;
+window.wp.components = wpComponents;
+window.wp.hooks = wpHooks;
+window.wp.url = wpUrl;
+window.wp.editSite = wpEditSite;
+window.wp.apiFetch = apiFetch;
+window.wp.date = wpDate;
+window.wp.domReady = domReady;
+window.wp.plugins = wpPlugins;
+window.wp.viewport = wpViewport;
+window.wp.richText = wpRichText;
+window.wp.editor = wpEditor;
+window.wp.coreData = wpCoreData;
+window.wp.tokenList = TokenList;
+window.wp.blob = wpBlob;
+window.React = react;
+window.ReactDOM = reactDom;
+
+function injectStyles(styles) {
+	const styleContainer = document.createElement('div');
+	styleContainer.innerHTML = styles;
+	document.head.appendChild(styleContainer);
+}
+
+function injectScripts(scripts) {
+	const scriptContainer = document.createElement('div');
+	const sanitizedScripts = scripts.replace(/\\"/g, "'");
+	scriptContainer.innerHTML = sanitizedScripts;
+	const scriptTags = scriptContainer.querySelectorAll('script');
+
+	scriptTags.forEach((scriptTag) => {
+		const newScript = document.createElement('script');
+
+		if (scriptTag.src) {
+			newScript.src = scriptTag.src;
+		} else {
+			const blob = new Blob([scriptTag.textContent], {
+				type: 'application/javascript',
+			});
+			const url = URL.createObjectURL(blob);
+			newScript.src = url;
+		}
+
+		if (scriptTag.id) {
+			newScript.id = scriptTag.id;
+		}
+
+		document.body.appendChild(newScript);
+	});
+}
 
 function Editor() {
 	const [blocks, setBlocks] = useState([]);
@@ -76,6 +160,12 @@ function Editor() {
 
 	useEffect(() => {
 		window.editor = editor;
+
+		injectStyles(__unstableResolvedAssets.styles);
+		injectScripts(__unstableResolvedAssets.scripts);
+
+		// dispatch(blocksStore).reapplyBlockTypeFilters();
+
 		registerCoreBlocks();
 		postMessage('onEditorLoaded');
 
